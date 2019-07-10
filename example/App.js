04ad1f8d58ae5e514a4c34d3c96b4994f1465d7b
unknown <myrnamares90@gmail.com>
2019-07-10 10:04:46 -0500

    se agregaron las funciones para marcar al ganador, boton para iniciar el juego, y las alerts que muestra quien gano

--- example/App.js
+++ example/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity,  } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, Button  } from 'react-native';
 import {MaterialCommunityIcons as Icon} from "react-native-vector-icons";
 
 export default class App extends React.Component{
@@ -26,21 +26,74 @@ export default class App extends React.Component{
         [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]
-      ]
+      ],
+      currentPlayer: 1,
     });
   }
 
+  //Returns 1 if player 1 won, -1 if Player 2 won, or a 0 if no one has won
+  getWinner = () => {
+    const NUM_TILES =3;
+    let arr = this.state.gameState;
+    let sum;
+
+    //Check rows...
+    for(let i = 0; i < NUM_TILES; i++){
+      sum = arr[i][0] + arr[i][1] + arr[i][2];
+      if(sum == 3){ return 1;}
+      else if (sum == -3){ return -1; }
+    }
+
+    //Check columns... 
+    for (let i = 0; i< NUM_TILES; i++){
+      sum = arr[0][i] + arr[1][i] + arr[2][i];
+      if (sum == 3){ return 1;}
+      else if (sum == -3){ return -1;}
+    }
+
+    //Check the diagonals...
+    sum = arr[0][0] + arr[1][1] + arr[2][2];
+    if (sum == 3){ return 1;}
+    else if (sum == -3){ return -1;}
+
+    sum = arr[2][0] + arr[1][1] + arr[0][2];
+    if (sum == 3){ return 1;}
+    else if (sum == -3){ return -1;}
+
+    //There are no winners...
+    return 0;
+  }
+
   onTilePress = (row, col) =>{
-    const currentPlayer = this.state.currentPlayer;
+    //Don't allow tiles to change...
+    let value = this.state.gameState[row][col];
+    if(value !== 0){return;}
+
+    //Grab current tile...
+    let currentPlayer = this.state.currentPlayer;
 
     //Set the correct tile...
-    const arr = this.state.gameState.slice();
+    let arr = this.state.gameState.slice();
     arr[row][col] = currentPlayer;
     this.setState({gameState: arr});
 
     //Switch to other player...
-    const nextPlayer = (currentPlayer == 1) ?-1: 1;
+    let nextPlayer = (currentPlayer == 1) ?-1: 1;
     this.setState({currentPlayer: nextPlayer});
+
+    //Check for winners...
+    let winner = this.getWinner();
+    if(winner == 1){
+      Alert.alert("Player 1 is the winner");
+      this.initializeGame();
+    } else if (winner == -1){
+      Alert.alert("Player 2 is the winner");
+      this.initializeGame();
+    }
+  }
+
+  onNewGamePress = () => {
+    this.initializeGame();
   }
 
   renderIcon = (row, col) => {
@@ -71,7 +124,7 @@ export default class App extends React.Component{
   
           {/*Are the views of every line of cat pictures*/}
         <View style={{flexDirection:"row"}}>
-        <TouchableOpacity onPress={() => this.onTilePress(1,0)} style={[styles.tile, {borderLeftWidth: 0,}]}>
+        <TouchableOpacity onPress={() => this.onTilePress(1, 0)} style={[styles.tile, {borderLeftWidth: 0,}]}>
           {this.renderIcon(1, 0)}
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.onTilePress(1,1)} style={[styles.tile, {}]}>
@@ -94,6 +147,9 @@ export default class App extends React.Component{
           {this.renderIcon(2, 2)}
         </TouchableOpacity>
         </View>
+
+        <View style={{paddingTop:50}}/>
+        <Button title="New Game" onPress={this.onNewGamePress}/>
       </View>
     );
   } 
